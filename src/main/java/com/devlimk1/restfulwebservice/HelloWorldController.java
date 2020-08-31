044f9e2d3a3408473af00e5f226a292e1b343130
DevLimk1 <kyenwon2@naver.com>
2020-08-31 23:22:00 +0900

    init, upload project

---
+++ src/main/java/com/devlimk1/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,9 @@
+package com.devlimk1.restfulwebservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //GET
+    //
+}
